7e95e8b162de54d39313ea172bfe4287b99e9963
Bartosz Pieczara <bartoszpieczara@Bartoszs-MacBook-Pro.local>
2018-11-18 12:44:18 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-kurs-youtube';
+export class AppComponent implements OnInit {
+
+  tasksList: Array<string> = [];
+  doneTasksList: Array<string> = [];
+
+  ngOnInit(): void {
+
+    this.tasksList = ['Sprzatanie kuwety', 'Nauka angulara', 'Podlewanie kwiatkow', 'Zakupy'];
+  }
+
+  add(task: string) {
+    this.tasksList.push(task);
+  }
+
+  markAsDone(task: string) {
+
+    this.doneTasksList.push(task);
+    this.remove(task);
+
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter(e => e !== task);
+  }
+
+  // inputText = 'Tekst';
+  // maxLength = 5;
+  // colorClass = 'color';
+  // logoUrl = '';
+
+  // isDisabled = true;
+
+  // constructor() {
+
+  //   setTimeout(() => {
+  //     this.isDisabled = false;
+  //   }, 3000);
+
+  // }
+
+
+  // change() {
+
+  //   this.inputText = 'Zmiana tekstu i koloru';
+  //   this.maxLength = 10;
+  //   this.colorClass = 'color2';
+  //   this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+
+  // }
+
 }
